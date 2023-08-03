81e654c9cda5f54de55faf9fb4c7aa0b81ff2e44
RegalRoy <regalroyacruz@gmail.com>
2023-08-03 13:46:08 -0700

    part1UploadPicDate

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -3,29 +3,38 @@ const controller = require("../controller/user.controller");
 const path = require("path");
 const express = require("express");
 
+const multer = require('multer');
 
 const bodyParser = require('body-parser');
-
-module.exports = function(app) {
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads_dates/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+const upload = multer({ storage: storage })
+module.exports = function (app) {
   // app.use(bodyParser.urlencoded({ extended: false }))
   app.use(bodyParser.urlencoded({ extended: true }));
 
 
-  app.use(function(req, res, next) {
+  app.use(function (req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
     );
     next();
   });
-  
+
   const uploadsFolderPath = path.resolve(__dirname, "../../uploads");
   app.use("/uploads", express.static(uploadsFolderPath));
 
   app.get("/api/test/all", controller.allAccess);
 
   app.get("/api/test/user", [authJwt.verifyToken], controller.userBoard);
- 
+
   app.get(
     "/api/test/mod",
     [authJwt.verifyToken, authJwt.isModerator],
@@ -47,5 +56,8 @@ module.exports = function(app) {
   app.delete("/api/test/getdate/:id", [authJwt.verifyToken], controller.deleteDate)
   app.delete("/api/test/getdate", [authJwt.verifyToken], controller.deleteAllDate)
   // app.put("/api/test/register/:id", [authJwt.verifyToken], controller.registerDate)
-
+  app.post("/api/test/postdate/pic", upload.single('photo'), [authJwt.verifyToken], controller.postPic)
+  app.get("/api/test/postdate/pic/:id",[authJwt.verifyToken], controller.getADatePic)
 };
+
+//get("http://localhost:8080/api/test/postdate/pic/"+picOwner
